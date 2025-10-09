af49af03d6b173669222c1079fa52f54c048f6e1
Leo Nistor <leo.nistor@gmail.com>
2025-10-09 16:53:08 +0300

    add alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,27 @@
 
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
+// https://github.com/Zastinian/astro-bun
+import bun from "@hedystia/astro-bun"
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: bun(),
+  output: "server",
+  devToolbar: { enabled: false },
+
   vite: {
     plugins: [tailwindcss()],
+    clearScreen: false,
+    server: {
+      host: "0.0.0.0",
+      watch: {
+        ignored: ["pocket/**"],
+      },
+    },
   },
+
+  integrations: [alpinejs()],
 })
