8a98722bc549fa46cdf22428e7d85f5fb958f421
Leo Nistor <leo.nistor@gmail.com>
2025-10-13 10:50:10 +0300

    layout, tailwind plugins, font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "astro/config"
+import { defineConfig, fontProviders } from "astro/config"
 // https://github.com/Zastinian/astro-bun
 import bun from "@hedystia/astro-bun"
 
@@ -19,9 +19,20 @@ export default defineConfig({
     server: {
       host: "0.0.0.0",
       watch: {
-        ignored: ["pocket/**"],
+        ignored: ["pocket/**", "TODO.txt", "import_data/*"],
       },
     },
+  },
+
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Work Sans",
+        weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        cssVariable: "--font-work-sans",
+      },
+    ],
   },
 
   integrations: [alpinejs({ entrypoint: "./src/alpine.ts" })],
