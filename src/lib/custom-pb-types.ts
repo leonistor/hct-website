6fb43a8f75501cf358dabe8bec29d26156a7c338
Leo Nistor <leo.nistor@gmail.com>
2025-10-26 05:20:42 +0200

    cleanup

--- src/lib/custom-pb-types.ts
+++
@@ -1,7 +0,0 @@
-// === start of custom type ===
-  // Mesaje.MesajeContact_extra.contact_extra
-  export type MesajeContact_extra = Array<{
- 
-  }>;
-  // === end of custom type ===
-
--- zod-pocketbase.config.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from "zod-pocketbase"
-
-export default {
-  ignore: ["_"],
-  output: "src/lib/pocketbase/schemas.ts",
-} satisfies Config
