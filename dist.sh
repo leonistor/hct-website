d208a696bb29b5d0ff2cdf59c55bba36f5736a60
Leo Nistor <leo.nistor@gmail.com>
2025-10-28 14:56:22 +0200

    fixish dist

--- dist.sh
+++ dist.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-PROJECT="hct"
+PROJECT="hct-pb-data"
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M")
 NAME="$PROJECT-$TIMESTAMP"
 
 mkdir -p build
-tar --exclude=./pocket/pb_data/backups -czf "build/$NAME.tar.gz" pocket/.pocketbase-version pocket/pb_data dist
+tar --exclude=./pocket/pb_data/backups -czf "build/$NAME.tar.gz" pocket/.pocketbase-version pocket/pb_data
 
 echo "Done archive at: build/$NAME.tar.gz"
 
-# get https://github.com/pocketbase/pocketbase/releases/download/v0.31.0/pocketbase_0.31.0_linux_amd64.zip
-# unzip pocketbase_0.31.0_linux_amd64.zip -x CHANGELOG.md LICENSE.md -d pocket/
+# !!! .env
+# bun run dev --allowed-hosts=pop-os.lan
+# tar xzvf hct-pb-data-2025-10-28_13-56.tar.gz --directory hct-website
